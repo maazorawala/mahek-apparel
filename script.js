eef92f91ae26597b07b5ce26ba0b0819fe697a7c
maazorawala <maazmaqbool995@gmail.com>
2021-05-27 21:03:11 +0530

    home-page-update
    home-page-update

--- script.js
+++ script.js
@@ -13,25 +13,6 @@ document.onreadystatechange = () =>{
 /**Loader function */
 
 
-/**opening and closing sidenav */
-let openNav = function () {
-    document.getElementById("sideNav").style.width = "700px";
-    document.getElementById("main").style.marginLeft = "700px";
-    
-}
-const closeNav = function() {
-    document.getElementById("sideNav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-/**opening and closing sidenav */
-
-/**Close sidenav with escape key function */
-document.addEventListener('keydown' , function(e){
-    if(e.key === 'Escape'){
-        closeNav()
-    }
-})
-/**Close sidenav with escape key function */
 
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
